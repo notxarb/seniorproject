ac877d37373ccedd950d1549b92e3981c0468ab7
Braxton Huggins <braxton@pagodabox.com>
2013-07-01 17:09:56 -0600

    more changes

--- js/navagation.js
+++ js/navagation.js
@@ -2,3 +2,9 @@ function NavagateTo(url)
 {
   document.location.href = url;
 }
+
+function toggle_display(element_id)
+{
+  element = document.getElementById(element_id);
+  element.style.display = element.style.display === 'none' ? '' : 'none';
+}

da3fa08c9504852586d737aef93253be5030c289
Braxton Huggins <braxton@pagodabox.com>
2013-07-02 19:37:52 -0600

    add bounds settings

--- js/equation.js
+++ js/equation.js
@@ -1,7 +1,20 @@
-var Equation = function Equation() {
-
+var Equation = function Equation(equation) {
+  this.equation = equation
+  this.generate_points();
+  this.generate_surfaces();
 }
 
 Equation.prototype = {
-  
+  generate_points: function generate_points() {
+
+  },
+  generate_surfaces: function generate_surfaces() {
+
+  },
+  draw: function draw() {
+
+  },
+  detect_asymptotes: function detect_asymptotes() {
+    
+  }
 }

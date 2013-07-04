af3a8af8ca2211e28d6a10693a07d20137d7248f
Braxton Huggins <braxton@pagodabox.com>
2013-07-03 19:06:33 -0600

    more updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
+
+var p_matrix = new PerspectiveMatrix();
+var mv_matrix = new TransformationMatrix();
+
 function on_page_load() {
+  mv_matrix.create_transformation_matrix();
+  p_matrix.create_perspective_matrix();
   initialize_canvas();
 }
 
 function initialize_canvas() {
   webGLStart()
+}
+
+function update_transformation_matrix() {
+  mv_matrix.create_transformation_matrix();
+  drawScene();
+}
+
+function update_perspective_matrix() {
+  p_matrix.create_perspective_matrix();
+  drawScene();
+}
+
+function update_ambient_lighting() {
+  drawScene();
+}
+
+function update_directional_lighing() {
+  drawScene();
+}
+
+function update_bounds() {
+  drawScene();
 }
---
+++ js/surface.js
@@ -0,0 +1,11 @@
+var Surface = function Surface(Surface) {
+  this.equation = equation
+  this.generate_points();
+  this.generate_surfaces();
+}
+
+Surface.prototype = {
+  draw: function draw() {
+    
+  }
+}
